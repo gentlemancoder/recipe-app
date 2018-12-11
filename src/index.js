26c3220d7a5f46a37fc9d873b6ee8fe895868d68
Chananya Jacobson <chananya.jacobson@gmail.comcd ~notepad .gitignorels ~al ~/.sshgit config --global user.email chananya.jacobson@gmail.com>
2018-12-11 12:05:41 -0500

    card rendering works

--- src/index.js
+++ src/index.js
@@ -1,125 +1,18 @@
-// import uuidv4 from 'uuid/v4'
-// import moment from 'moment'
-
-
-renderRecipes()
-
-document.querySelector('#"add-recipe').addEventListener('click', (e) => {
-    const id = createRecipe()
-    location.assign('/recipe.html#' +id)
-})
-
-document.querySelector('.searchbox').addEventListener('input', (e) => {
-    setFilters({
-        searchText: e.target.value
-    })  
-    renderRecipes()
-})
-
-window.addEventListener('storage', (e) => {
-    if (e.key === 'recipes'){
-        loadRecipes()
-        renderRecipes()
-    }
-})
-
-
-const searchFilter = {
-REsearchText: '',
+let recipe = {
+    id: '123',
+    title: 'Some Food',
+    recipe: 'Do this. Then That',
+    ingredients: [{
+        ingredientName: '',
+        inStock: true
+    },
+    {
+        ingredientName: '',
+        inStock: true
+    }]
 }
 
-const getFilter = () => searchFilter
-
-const setFilter = (updates) => {
-    if (typeof updates.searchText === 'string'){
-        searchFilter.searchText = updates.searchText
-    }
-}
-
-
-// Generate the DOM structure for a recipe card
-const generateRecipeCardDOM = (recipe) => {
-    const recipeEl = document.createElement('a')
-    const titleEl = document.createElement('p')
-    const ingredientEl = document.createElement('p')
-
-    // Set up the note title text
-    if (recipe.title.length > 0 ){
-        titleEl.textContent = recipe.title
-    } else {
-        titleEl.textContent = 'Unnamed Recipe'
-    }
-    // titleEl.classList.add('list-item__title')
-    recipeEl.appendChild(titleEl)
-
-    // Setup the link
-    recipeEl.setAttribute("href", `/edit.html#${recipe.id}`)
-    // recipeEl.classList.add('list-item')
-
-    // Setup the ingredient status message
-    ingredientEl.textContent = getEnoughIngredients()
-    // ingredientEl.classList.add('list-item__subtitle')
-    recipeEl.appendChild(ingredientEl)
-
-    return recipeEl
-}
-
-
-const createRecipe = () => {
-    const id = uuidv4()
-
-    recipes.push({
-        id: id,
-        title: '',
-        recipe: '',
-        ingredients: {
-            ingredientName: '',
-            inStock: false
-        },
-    })
-    saveRecipes()
-
-    return id
-}
-
-// Render application notes
-const renderRecipes = () => {
-    const notesEl = document.querySelector('#notes')
-    const filters = getFilters()
-    const notes = sortNotes(filters.sortBy)
-    const filteredNotes = notes.filter( (note) => note.title.toLowerCase().includes(filters.searchText.toLowerCase()))
-
-    notesEl.innerHTML = '';
-    
-    if(filteredNotes.length > 0){
-        filteredNotes.forEach( (note) => {
-            const noteEl = generateNoteDOM(note)
-            notesEl.appendChild(noteEl)
-        })    
-    } else {
-        const emptyMessage = document.createElement('p')
-        emptyMessage.textContent = 'No notes to show'
-        emptyMessage.classList.add('empty-message')
-        notesEl.appendChild(emptyMessage)
-    }
-}
-/*
-const initializeEditPage = (noteId) => {
-    const titleElement = document.querySelector('#note-title')
-    const timeElement = document.querySelector('#timestamp')
-    const bodyElement = document.querySelector('#note-body')
-    const notes = getNotes()
-    const note = notes.find((note) => note.id === noteId) 
-    
-    if (!note) {
-        location.assign('/index.html')
-    }
-    
-    titleElement.value = note.title
-    timeElement.textContent = `Last edited ${moment(note.updatedAt).fromNow()}`
-    bodyElement.value = note.body    
-}*/
-
+console.log(recipe);
 
 const getEnoughIngredients = (ingredients) => {
     let doHave = false;
@@ -141,3 +34,153 @@ const getEnoughIngredients = (ingredients) => {
         return 'You have everything you need to start cooking!'
     }
 }
+const renderCard = (recipe) => {
+    const cardEl = document.querySelector('.cards');
+    
+    const recipeEl = document.createElement('a')
+    const titleEl = document.createElement('p')
+    const ingredientEl = document.createElement('p')
+
+    if (recipe.title.length > 0 ){
+        titleEl.textContent = recipe.title
+    } else {
+        titleEl.textContent = 'Unnamed Recipe'
+    }
+    // titleEl.classList.add('list-item__title')
+    recipeEl.appendChild(titleEl)
+
+    // Setup the link
+    recipeEl.setAttribute("href", `/edit.html#${recipe.id}`)
+    // recipeEl.classList.add('list-item')
+
+    // Setup the ingredient status message
+    ingredientEl.textContent = getEnoughIngredients(recipe.ingredients)
+    
+    recipeEl.appendChild(ingredientEl)
+
+    cardEl.appendChild(recipeEl)
+}
+renderCard(recipe)
+
+
+// const searchFilter = {
+// searchText: '',
+// }
+// const getFilter = () => searchFilter
+// // import uuidv4 from 'uuid/v4'
+// // import moment from 'moment'
+
+// const renderRecipes = () => {
+//     const notesEl = document.querySelector('#notes')
+//     const filters = getFilter()
+//     const recipes = [''] // sortNotes(filters.sortBy)
+//     const filteredNotes = recipes.filter( (recipe) => recipe.title.toLowerCase().includes(filters.searchText.toLowerCase()))
+
+//     notesEl.innerHTML = '';
+    
+//     if(filteredNotes.length > 0){
+//         filteredNotes.forEach( (note) => {
+//             const noteEl = generateNoteDOM(note)
+//             notesEl.appendChild(noteEl)
+//         })    
+//     } else {
+//         const emptyMessage = document.createElement('p')
+//         emptyMessage.textContent = 'No notes to show'
+//         emptyMessage.classList.add('empty-message')
+//         notesEl.appendChild(emptyMessage)
+//     }
+// }
+
+// renderRecipes()
+
+// document.querySelector('#"add-recipe').addEventListener('click', (e) => {
+//     const id = createRecipe()
+//     location.assign('/recipe.html#' +id)
+// })
+
+// document.querySelector('.searchbox').addEventListener('input', (e) => {
+//     setFilters({
+//         searchText: e.target.value
+//     })  
+//     renderRecipes()
+// })
+
+// window.addEventListener('storage', (e) => {
+//     if (e.key === 'recipes'){
+//         loadRecipes()
+//         renderRecipes()
+//     }
+// })
+
+
+
+
+// const setFilter = (updates) => {
+//     if (typeof updates.searchText === 'string'){
+//         searchFilter.searchText = updates.searchText
+//     }
+// }
+
+
+// // Generate the DOM structure for a recipe card
+// const generateRecipeCardDOM = (recipe) => {
+//     const recipeEl = document.createElement('a')
+//     const titleEl = document.createElement('p')
+//     const ingredientEl = document.createElement('p')
+
+//     // Set up the note title text
+//     if (recipe.title.length > 0 ){
+//         titleEl.textContent = recipe.title
+//     } else {
+//         titleEl.textContent = 'Unnamed Recipe'
+//     }
+//     // titleEl.classList.add('list-item__title')
+//     recipeEl.appendChild(titleEl)
+
+//     // Setup the link
+//     recipeEl.setAttribute("href", `/edit.html#${recipe.id}`)
+//     // recipeEl.classList.add('list-item')
+
+//     // Setup the ingredient status message
+//     ingredientEl.textContent = getEnoughIngredients()
+//     // ingredientEl.classList.add('list-item__subtitle')
+//     recipeEl.appendChild(ingredientEl)
+
+//     return recipeEl
+// }
+
+
+// const createRecipe = () => {
+//     const id = uuidv4()
+
+//     recipes.push({
+//         id: id,
+//         title: '',
+//         recipe: '',
+//         ingredients: {
+//             ingredientName: '',
+//             inStock: false
+//         },
+//     })
+//     saveRecipes()
+
+//     return id
+// }
+
+// /*
+// const initializeEditPage = (noteId) => {
+//     const titleElement = document.querySelector('#note-title')
+//     const timeElement = document.querySelector('#timestamp')
+//     const bodyElement = document.querySelector('#note-body')
+//     const notes = getNotes()
+//     const note = notes.find((note) => note.id === noteId) 
+    
+//     if (!note) {
+//         location.assign('/index.html')
+//     }
+    
+//     titleElement.value = note.title
+//     timeElement.textContent = `Last edited ${moment(note.updatedAt).fromNow()}`
+//     bodyElement.value = note.body    
+// }*/
+
