9aa78a98b1b68825545adb3f847e5d54a0168b83
Chananya Jacobson <chananya.jacobson@gmail.comcd ~notepad .gitignorels ~al ~/.sshgit config --global user.email chananya.jacobson@gmail.com>
2018-12-10 20:39:59 -0500

    imported major functional elements from notes-app

--- src/index.js
+++ src/index.js
@@ -1,5 +1,42 @@
-import uuidv4 from 'uuid/v4'
-import moment from 'moment'
+// import uuidv4 from 'uuid/v4'
+// import moment from 'moment'
+
+renderNotes()
+
+document.querySelector('#"add-recipe').addEventListener('click', (e) => {
+    const id = createRecipe()
+    location.assign('/recipe.html#' +id)
+})
+
+document.querySelector('#search-text').addEventListener('input', (e) => {
+    setFilters({
+        searchText: e.target.value
+    })  
+    renderNotes()
+})
+
+
+window.addEventListener('storage', (e) => {
+    if (e.key === 'recipes'){
+        loadRecipes()
+        renderNotes()
+    }
+})
+
+
+
+const filters = {
+    searchText: '',
+}
+
+const getFilters = () => filters
+
+const setFilters = (updates) => {
+    if (typeof updates.searchText === 'string'){
+        filters.searchText = updates.searchText
+    }
+}
+
 
 // Generate the DOM structure for a note
 const generateRecipeCardDOM = (recipe) => {
@@ -46,3 +83,43 @@ const createRecipe = () => {
     return id
 }
 
+// Render application notes
+const renderRecipes = () => {
+    const notesEl = document.querySelector('#notes')
+    const filters = getFilters()
+    const notes = sortNotes(filters.sortBy)
+    const filteredNotes = notes.filter( (note) => note.title.toLowerCase().includes(filters.searchText.toLowerCase()))
+
+    notesEl.innerHTML = '';
+    
+    if(filteredNotes.length > 0){
+        filteredNotes.forEach( (note) => {
+            const noteEl = generateNoteDOM(note)
+            notesEl.appendChild(noteEl)
+        })    
+    } else {
+        const emptyMessage = document.createElement('p')
+        emptyMessage.textContent = 'No notes to show'
+        emptyMessage.classList.add('empty-message')
+        notesEl.appendChild(emptyMessage)
+    }
+}
+/*
+const initializeEditPage = (noteId) => {
+    const titleElement = document.querySelector('#note-title')
+    const timeElement = document.querySelector('#timestamp')
+    const bodyElement = document.querySelector('#note-body')
+    const notes = getNotes()
+    const note = notes.find((note) => note.id === noteId) 
+    
+    if (!note) {
+        location.assign('/index.html')
+    }
+    
+    titleElement.value = note.title
+    timeElement.textContent = `Last edited ${moment(note.updatedAt).fromNow()}`
+    bodyElement.value = note.body    
+}*/
+
+// Generate the last edited message
+const generateLastEdited = (timestamp) => `Last edited ${moment(timestamp).fromNow()}`
